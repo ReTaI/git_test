e59a7047049610df5df79d90146b19f5cb271f63
ReTaI <retai01@mail.ru>
2020-03-15 04:11:22 +0300

    находятся палиндромы с нечетным количеством символов

--- test.py
+++ test.py
@@ -3,7 +3,7 @@ maxlen=0
 for l in range (0,string.__len__()):
     k=0
     len=0
-    while(l-k>0) and (l + k < string.__len__()):
+    while(l-k>=0) and (l + k < string.__len__()):
         if string[l-k]==string[l+k]:
             len+=1
             if(len>maxlen):
