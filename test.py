39f86a4d93a4581fd1cd86fcc177bfb41c214416
ReTaI <retai01@mail.ru>
2020-03-15 04:24:25 +0300

    Версия 1.0, находятся все палиндромы, необходимо тестирование

--- test.py
+++ test.py
@@ -1,13 +1,25 @@
 string=input()
 maxlen=0
-for l in range (0,string.__len__()):
-    k=0
+for i in range (0,string.__len__()):
+    left=i
+    right=i+1
     len=0
-    while(l-k>=0) and (l + k < string.__len__()):
-        if string[l-k]==string[l+k]:
-            len+=1
-            if(len>maxlen):
-                maxlen = len
-                longest=string[l-k:l+k+1]
-        k=k+1
+    while(left>=0) and (right<string.__len__()) and (string[left]==string[right]):
+        len+=1
+        if(len>maxlen):
+            maxlen=len
+            longest=string[left:right+1]
+        left-=1
+        right+=1
+for i in range (0,string.__len__()):
+    left=i-1
+    right=i+1
+    len=0
+    while(left>=0) and (right<string.__len__()) and (string[left]==string[right]):
+        len+=1
+        if(len>maxlen):
+            maxlen=len
+            longest=string[left:right+1]
+        left-=1
+        right+=1
 print(longest)
